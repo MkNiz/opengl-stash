305521221099f313c750e66c7e5af3723b254c00
Michael Nizzari <michael.nizzari@gmail.com>
2017-08-15 17:29:56 -0400

    01 - Window - glad include, window hints, window creation

--- 01-window/Source.cpp
+++ 01-window/Source.cpp
@@ -1,1 +1,22 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "MyOpenGLWindow", NULL, NULL);
+	if (window == NULL) {
+		cout << "Window creation failed" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	return 0;
+}
